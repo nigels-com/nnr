89d7a5f6af529a39035ae738c42ba9bf34a6f826
Nigel Stewart <nigels@nigels.com>
2019-03-15 19:54:06 +1000

    nnr front-end for NNR sequences

---
+++ nnr.cpp
@@ -0,0 +1,32 @@
+//
+// parameters: n l
+//
+// n is the size of the alphabet
+// l is the length of the sequences
+
+#include <iostream>
+#include <cassert>
+
+#include "nnr.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc!=3)
+    {
+        cerr << argv[0] << " n l" << endl;
+        return EXIT_FAILURE;
+    }
+
+    int n = atoi(argv[1]);
+    int l = atoi(argv[2]);
+
+    cerr << "n = " << n << endl;
+    cerr << "l = " << l << endl;
+    cerr << endl;
+
+    NNRprintSequences(n, l);
+
+    return EXIT_SUCCESS;
+}
