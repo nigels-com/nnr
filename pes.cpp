020e7df1b2432b1b8f8b7007d9ffe03a576a85bc
Nigel Stewart <nigels@nigels.com>
2019-03-27 21:19:16 +1000

    Experimenting with contiguous permutations in memory and random shuffling for pes - no speedup

--- pes.cpp
+++ pes.cpp
@@ -14,149 +14,7 @@
 
 using namespace std;
 
-// Output sequence as alpha string
-
-inline std::ostream &
-operator<<(std::ostream &os, const std::vector<uint8_t> & str)
-{
-    string out;
-    out.resize(str.size());
-    size_t j = 0;
-    for (const auto i : str) out[j++] = 'a' + char(i);
-    os << out;
-    return os;
-}
-
-static const size_t   factorialTableSize = 30;
-static const uint64_t factorialTable[1+factorialTableSize] =
-{
-    1lu,
-    1lu,
-    1lu*2,
-    1lu*2*3,
-    1lu*2*3*4,
-    1lu*2*3*4*5,
-    1lu*2*3*4*5*6,
-    1lu*2*3*4*5*6*7,
-    1lu*2*3*4*5*6*7*8,
-    1lu*2*3*4*5*6*7*8*9,
-    1lu*2*3*4*5*6*7*8*9*10,
-    1lu*2*3*4*5*6*7*8*9*10*11,
-    1lu*2*3*4*5*6*7*8*9*10*11*12,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25*26,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25*26*27,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25*26*27*28,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25*26*27*28*29,
-    1lu*2*3*4*5*6*7*8*9*10*11*12*13*14*15*16*17*18*19*20*21*22*23*24*25*26*27*28*29*30
-};
-
-template<typename T>
-T factorial(const T n)
-{
-    if (n<=factorialTableSize)
-        return factorialTable[n];
-
-    T ret = factorialTable[factorialTableSize];
-    for (T i = factorialTableSize+1; i <= n; ++i)
-        ret *= i;
-    return ret;
-}
-
-inline
-std::vector<uint8_t>
-permutation(const uint64_t n, const uint64_t p, const uint64_t k = 0)
-{
-    //
-    // Check that k is not too large
-    //
-
-    assert(k<=n);
-    if (k>n)
-        return std::vector<uint8_t>();
-
-    //
-    // Do it
-    //
-
-    const uint64_t length = k ? k : n;
-
-    //
-    // Create 'abcde...'
-    //
-
-    std::vector<uint8_t> perm;
-    for (uint64_t c=0; c<n; c++)
-        perm.push_back(n-1-c);
-
-    //
-
-    std::vector<uint8_t> out;
-    uint64_t seed = p;
-
-    while (out.size()<length)
-    {
-        const uint64_t choices = perm.size();
-        const uint64_t select = seed%choices;
-        seed = seed/choices;
-
-        out.push_back(perm[select]);
-        perm.erase(perm.begin()+select);
-    }
-
-    return out;
-}
-
-vector<std::vector<uint8_t>>
-permutations(const uint64_t n, const uint64_t k = 0)
-{
-    //
-    // Default behaviour is when k
-    // is assumed to be n
-    //
-    // k is the 'length' of desired
-    // permutations.  k!=n implies
-    // 'arrangements'.
-    //
-
-    if (k==0 || k==n)
-    {
-        const uint64_t size = factorial(n);
-        vector<std::vector<uint8_t>> perms;
-        perms.reserve(size);
-
-        for (uint64_t p=0; p<size; p++)
-            perms.push_back(permutation(n,p));
-
-        return perms;
-    }
-
-    //
-    // Do every arrangement of n
-    // select k.
-    //
-
-    const uint64_t size = factorial(n)/factorial(n-k);
-    vector<std::vector<uint8_t>> perms;
-    perms.reserve(size);
-
-    for (uint64_t p=0; p<size; p++)
-        perms.push_back(permutation(n, p, k));
-
-    return perms;
-}
+// Input sequence as alpha string
 
 std::vector<uint8_t>
 fromString(const char *str)
@@ -178,26 +36,157 @@ fromString(const char *str)
     return tmp;
 }
 
-// p is embedded in [begin, end)
+// Output sequence as alpha string
 
-bool
-embeddedSequence(const uint8_t * begin, const uint8_t * end, const std::vector<uint8_t> & p)
+inline std::ostream &
+operator<<(std::ostream &os, const std::vector<uint8_t> & str)
 {
-    const size_t size = end-begin;
+    string out;
+    out.resize(str.size());
+    size_t j = 0;
+    for (const auto i : str) out[j++] = 'a' + char(i);
+    os << out;
+    return os;
+}
 
-    if (p.size() > size)
+// Factorial
+
+template<typename T>
+T factorial(const T n)
+{
+    T ret = 1;
+    for (T i = 2; i <= n; ++i)
+        ret *= i;
+    return ret;
+}
+
+inline
+bool
+permutation(uint8_t * buffer, const uint64_t n, const uint64_t p, const uint64_t k = 0)
+{
+    //
+    // Check that k is not too large
+    //
+
+    assert(k<=n);
+    if (k>n)
         return false;
 
-    if (p.size() == size && !memcmp(begin, &p[0], size))
+    //
+    // Do it
+    //
+
+    const uint64_t length = k ? k : n;
+
+    //
+    // Create 'abcde...'
+    //
+
+    std::vector<uint8_t> perm;
+    for (uint64_t c=0; c<n; c++)
+        perm.push_back(n-1-c);
+
+    //
+
+    std::vector<uint8_t> out;
+    uint64_t seed = p;
+
+    for (uint8_t *i = buffer; i<buffer+length; ++i)
+    {
+        const uint64_t choices = perm.size();
+        const uint64_t select = seed%choices;
+        seed = seed/choices;
+
+        *i = perm[select];
+        perm.erase(perm.begin()+select);
+    }
+    return true;
+}
+
+bool
+permutations(std::vector<uint8_t> & buffer, const uint64_t n, const uint64_t k = 0)
+{
+    //
+    // Default behaviour is when k
+    // is assumed to be n
+    //
+    // k is the 'length' of desired
+    // permutations.  k!=n implies
+    // 'arrangements'.
+    //
+
+    if (k==0 || k==n)
+    {
+        const uint64_t size = factorial(n);
+        buffer.resize(size*n);
+
+        uint8_t *i = &buffer[0];
+        for (uint64_t p=0; p<size; p++, i += n)
+            permutation(i, n, p);
+
+        return true;
+    }
+
+    //
+    // Do every arrangement of n
+    // select k.
+    //
+
+    const uint64_t size = factorial(n)/factorial(n-k);
+    buffer.resize(size*k);
+
+    uint8_t *i = &buffer[0];
+    for (uint64_t p=0; p<size; p++, i += k)
+        permutation(i, n, p, k);
+
+    return true;
+}
+
+inline bool
+shuffle(std::vector<uint8_t> & buffer, const uint64_t n, const uint64_t k = 0)
+{
+    const uint64_t length = k>0 ? k : n;
+
+    uint8_t *begin = &buffer[0];
+    uint8_t *end   = begin + buffer.size();
+
+    uint64_t m = buffer.size()/length;
+    vector<uint64_t> tmp(length);
+
+    for (uint8_t *i = begin; i<end; i += length, --m)
+    {
+        uint8_t *j = i + (rand()%m)*length;
+        memcpy(&tmp[0], i,       length);
+        memcpy(i,       j,       length);
+        memcpy(j,       &tmp[0], length);
+    }
+
+    return true;
+}
+
+// [nBegin, nEnd] is embedded in [hBegin, hEnd)
+
+bool
+embeddedSequence(
+    const uint8_t * hBegin, const uint8_t * hEnd,
+    const uint8_t * nBegin, const uint8_t * nEnd)
+{
+    const size_t hSize = hEnd-hBegin;
+    const size_t nSize = nEnd-nBegin;
+
+    if (nSize > nSize)
+        return false;
+
+    if (nSize == nSize && !memcmp(hBegin, nBegin, nSize))
         return true;
 
-    unsigned int n = 0;
-    for (const uint8_t * i = begin; i != end; ++i)
+    const uint8_t * j = nBegin;
+    for (const uint8_t * i = hBegin; i != hEnd; ++i)
     {
-        if (*i==p[n])
+        if (*i==*j)
         {
-            n++;
-            if (n==p.size())
+            j++;
+            if (j==nEnd)
             {
                 return true;
             }
@@ -229,14 +218,16 @@ int main(int argc, char *argv[])
     cerr << endl;
     #endif
 
-    vector<std::vector<uint8_t>> criteria;
+    std::vector<uint8_t> buffer;
     if (k>0)
     {
-        criteria = permutations(n, k);
+        permutations(buffer, n, k);
+        shuffle(buffer, n, k);
     }
     else
     {
-        criteria = permutations(n);
+        permutations(buffer, n);
+        shuffle(buffer, n);
     }
 
     const size_t size = k>0 ? k : n;
@@ -258,8 +249,11 @@ int main(int argc, char *argv[])
         #endif
 
         std::vector<uint8_t> s = fromString(line.c_str());
-        for (const auto & i : criteria)
-            if (!embeddedSequence(&s[0], &s[0]+s.size(), i))
+
+        const uint8_t * begin = &buffer[0];
+        const uint8_t * end   = begin + buffer.size();
+        for (const uint8_t *i = begin; i<end; i+=size)
+            if (!embeddedSequence(&s[0], &s[0]+s.size(), i, i + size))
             {
                 goto fail;
             }
